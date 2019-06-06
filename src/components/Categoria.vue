e4722315fcf05685f33c70df9bf568d584243214
Joabe Ramone <joab1995@hotmail.com>
2019-06-06 15:48:08 -0400

    Foi colocado os alerts das operações de sucesso e erros. E foi corrigido a paginação da tabelas que não estava funcionando.

--- src/components/Categoria.vue
+++ src/components/Categoria.vue
@@ -1,19 +1,9 @@
 <template>
   <div class="row">
-
     <b-container class="col-md-6 offset-3" fluid>
-      <!--BARRA DE PESQUISA-->
-
-      <b-form-group label-cols-sm="3" class="mb-0">
-      </b-form-group>
-
-      <!--BOTAO PARA CADASTRAR CATEGORIA-->
-      <b-card  class="card" bg-variant="dark" text-variant="white">
-
-
+      <b-card class="card" bg-variant="dark" text-variant="white">
         <h2 class="titulo-tabela">Tabela de categorias</h2>
-
-
+        <!--BOTAO PARA CADASTRAR CATEGORIA-->
         <div class="botao-cadastro">
           <b-input-group-append>
             <b-button class="botao-criar-categoria"
@@ -23,53 +13,47 @@
           </b-input-group-append>
 
         </div>
-
-        <!--:filter="filter"-->
         <!--TABELA-->
-        <b-table class="hover"
-                 :items="corpoDaCategoria"
-                 :fields="campoParaColunasDaCategoria"
-                 :sort-by.sync="sortBy"
-                 :sort-desc.sync="sortDesc"
-                 @filtered="onFiltered">
+        <div style="height: 350px">
+          <b-table class="hover"
+                   :items="corpoDaCategoria"
+                   :fields="campoParaColunasDaCategoria"
+                   :sort-by.sync="sortBy"
+                   :sort-desc.sync="sortDesc"
+                   :current-page="currentPage"
+                   @filtered="onFiltered(item)"
+                   :per-page="perPage">
+            <template slot="ação" slot-scope="row">
+              <!--BOTAO PARA ATUALIZAR CATEGORIA-->
+              <b-button v-b-modal.modal-editar
+                        title="Editar Categoria"
+                        class="botao-categoria"
+                        variant="outline-primary"
+                        @click="adicionarValoresDaLinhaClicada(row.item, row.index)">
+                <i class="fas fa-edit"></i>
+              </b-button>
+              <!--BOTAO PARA EXCLUIR CATEGORIA-->
+              <b-button v-b-modal.modal-excluir
+                        title="Excluir Categoria"
+                        class="botao-categoria"
+                        variant="outline-primary"
+                        @click="mandaValorDaLinhaParaModalCategoria(row.item)">
+                <i class="fas fa-trash-alt"></i>
+              </b-button>
+              <!--BOTAO PARA CADASTRAR PRODUTO-->
+              <b-button title="Adicionar Produto"
+                        class="botao-categoria"
+                        variant="outline-primary"
+                        @click="routerParaProdutos(adicionarValoresDaLinhaClicada(row.item, row.index))">
+                <i class="fas fa-archive"></i>
+              </b-button>
+            </template>
+          </b-table>
 
-          <template slot="ação" slot-scope="row">
-
-            <!--BOTAO PARA ATUALIZAR CATEGORIA-->
-            <b-button v-b-modal.modal-editar
-                      title="Editar Categoria"
-                      class="botao-categoria"
-                      variant="outline-primary"
-                      @click="adicionarValoresDaLinhaClicada(row.item, row.index)">
-              <i class="fas fa-edit"></i>
-            </b-button>
-
-            <!--BOTAO PARA EXCLUIR CATEGORIA-->
-            <b-button v-b-modal.modal-excluir
-                      title="Excluir Categoria"
-                      class="botao-categoria"
-                      variant="outline-primary"
-                      @click="mandaValorDaLinhaParaModalCategoria(row.item)"
-            >
-              <i class="fas fa-trash-alt"></i>
-            </b-button>
-
-            <!--BOTAO PARA CADASTRAR PRODUTO-->
-            <b-button title="Adicionar Produto"
-                      class="botao-categoria"
-                      variant="outline-primary"
-                      @click="routerParaProdutos(adicionarValoresDaLinhaClicada(row.item, row.index))"
-
-            >
-              <i class="fas fa-archive"></i>
-            </b-button>
-
-          </template>
-
-        </b-table>
-
+        </div>
+        <!--BARRA DE PAGINAS-->
         <div class="barra-de-paginas">
-          <b-row  >
+          <b-row>
             <b-col md="6" class="my-1">
               <b-pagination
                 v-model="currentPage"
@@ -79,20 +63,17 @@
               ></b-pagination>
             </b-col>
           </b-row>
-
         </div>
-
-
+        <!--BOTAO VOLTAR-->
         <div class="botao-cadastro">
-          <b-button  variant="danger">Voltar</b-button>
+          <b-button variant="danger">Voltar</b-button>
         </div>
       </b-card>
     </b-container>
 
-
     <!--MODAL-ATUALIZAR-->
     <div>
-      <b-modal id="modal-editar" title="Atualizar Categoria" hide-footer ref="modal-atualizar-categorias">
+      <b-modal id="modal-editar" title="Atualizar Categoria" hide-footer ref="modal-atualizar-categoria">
         <div>
           <b-form @submit="atualizarNomeCategoria" @reset="limparInputDoModal">
             <b-form-group
@@ -104,19 +85,19 @@
                             type="text"
                             required
                             placeholder="Digite um categorias"
-                            v-model="categoria.nome"
-              ></b-form-input>
+                            v-model="categoria.nome">
+              </b-form-input>
             </b-form-group>
             <div class="grupo-botao-modal">
               <b-button type="reset"
                         variant="danger"
-                        @click="fecharModal(1)"
-              >Cancelar
+                        @click="fecharModal(1)">
+                Cancelar
               </b-button>
               <b-button type="submit"
                         variant="success"
-                        @click=""
-              >Atualizar
+                        @click="">
+                Atualizar
               </b-button>
             </div>
           </b-form>
@@ -130,44 +111,61 @@
           <b-form @submit="cadastrarCategoria" @reset="limparInputDoModal">
             <b-form-group
               label="Nome da Categoria: "
-              label-for="input-1"
               description="Não coloque nome inapropriado.">
-              <b-form-input type="text"
+
+              <b-form-input id="input-cadastro"
+                            type="text"
                             required
                             placeholder="Digite um nome para a categoria"
-                            v-model="categoria.nome"
-              ></b-form-input>
+                            v-model="categoria.nome">
+              </b-form-input>
             </b-form-group>
-
-
             <div class="grupo-botao-modal">
               <b-button type="reset" variant="danger" @click="fecharModal(2)">Cancelar</b-button>
               <b-button type="submit" variant="success">Pronto</b-button>
             </div>
-
           </b-form>
         </div>
       </b-modal>
     </div>
-
     <!--MODAL EXCLUIR-->
     <div>
       <b-modal id="modal-excluir" title="Excluir Categoria" hide-footer ref="modal-excluir-categoria">
         <div>
           <b-form @submit="removerCategoria()" @reset="limparInputDoModal">
-            <p>Deseja mesmo excluir a categoria {{valorParaSerExcluido.nome}} ?</p>
-
+            <p>Deseja mesmo excluir a categoria {{categoria.nome}} ?</p>
             <div class="grupo-botao-modal">
               <b-button type="reset" variant="danger" @click="fecharModal(3)">Cancelar</b-button>
               <b-button type="submit" variant="success">Pronto</b-button>
             </div>
-
           </b-form>
         </div>
       </b-modal>
     </div>
-
-
+    <!--ALERT SUCESSO-->
+    <div style="position: absolute;right: 10px;top: 10px;z-index: 100000">
+      <b-alert
+        :show="alertSucesso"
+        dismissible
+        variant="success"
+        @dismissed="alertSucesso=0"
+        @dismiss-count-down="setSegundos">
+        <p v-show="flagSalvar">Categoria salva com sucesso!</p>
+        <p v-show="flagExcluir">Categoria Excluida com sucesso!</p>
+        <p v-show="flagAtualizar">Categoria Atualizada com sucesso!</p>
+      </b-alert>
+    </div>
+    <!--ALERT ERROR-->
+    <div style="position: absolute;right: 10px;top: 10px;z-index: 100000">
+      <b-alert
+        :show="alertError"
+        dismissible
+        variant="danger"
+        @dismissed="alertError=0"
+        @dismiss-count-down="setSegundos">
+        <p v-model="mensagem">{{mensagem}}</p>
+      </b-alert>
+    </div>
   </div>
 </template>
 
@@ -179,22 +177,23 @@
       return {
         totalRows: 1,
         currentPage: 1,
-        perPage: 5,
+        perPage: 4,
         sortBy: 'age',
         sortDesc: false,
         fields: ['first_name', 'last_name', 'show_details'],
         corpoDaCategoria: [],
         campoParaColunasDaCategoria: [{key: 'nome', sortable: true}, {key: 'ação'}],
-        valorParaSerExcluido: {
-          id: '',
-          nome: ''
-        },
-        valorDaCategoriaAtualizado: '',
-        nomeDaCategoriaParaCadastrar: '',
         categoria: {
           id: '',
           nome: '',
-        }
+        },
+        mensagem: '',
+        dismissSecs: 10,
+        alertSucesso: 0,
+        alertError: 0,
+        flagSalvar: false,
+        flagExcluir: false,
+        flagAtualizar: false
       }
     },
     created() {
@@ -202,9 +201,10 @@
     },
     methods: {
       buscarCategorias() {
-        this.$http.get('http://localhost:8050/categorias').then(response => {
+        this.$http.get('http://localhost:8082/categorias').then(response => {
           this.corpoDaCategoria = response.body;
-          console.log(this.corpoDaCategoria)
+          this.totalRows = this.corpoDaCategoria.length;
+
         });
       },
       fecharModal(opcaoDeModal) {
@@ -215,104 +215,117 @@
         } else if (opcaoDeModal === 3) {
           this.$refs['modal-excluir-categoria'].hide()
         }
-
       },
-      limparInputDoModal(evt) {
-        evt.preventDefault();
-        this.valorDaCategoriaAtualizado = '';
-        this.nomeDaCategoriaParaCadastrar = '';
+      limparInputDoModal() {
+        this.categoria.nome = '';
       },
       adicionarValoresDaLinhaClicada(categoriaDaLinhaClicadaDoBotaoEditar) {
         this.categoria.id = categoriaDaLinhaClicadaDoBotaoEditar.id;
         this.categoria.nome = categoriaDaLinhaClicadaDoBotaoEditar.nome;
       },
-      cadastrarCategoria(evt) {
-        evt.preventDefault();
-        console.log(this.categoria.nome);
+      cadastrarCategoria() {
+        this.resetAlert();
         this.$http.post('http://localhost:8082/categorias', this.categoria).then(response => {
-          alert("Categoria Salva!");
           this.fecharModal(2);
+          this.limparInputDoModal();
           this.buscarCategorias();
+          this.showAlertSucesso(1);
         }, response => {
-
+          this.mensagem = response.body.message;
+          this.setSegundosError(4);
         });
-
       },
       mandaValorDaLinhaParaModalCategoria(categoriaClicadaDaLinhaDoBotaoExcluir) {
-        this.valorParaSerExcluido.id = categoriaClicadaDaLinhaDoBotaoExcluir.id;
-        this.valorParaSerExcluido.nome = categoriaClicadaDaLinhaDoBotaoExcluir.nome;
+        this.categoria.id = categoriaClicadaDaLinhaDoBotaoExcluir.id;
+        this.categoria.nome = categoriaClicadaDaLinhaDoBotaoExcluir.nome;
       },
       removerCategoria() {
-        console.log(this.valorParaSerExcluido.id);
-
-        this.$http.delete('http://localhost:8082/categorias/' + this.valorParaSerExcluido.id).then(response => {
-          alert("Categoria Excluida!");
+        this.resetAlert();
+        this.$http.delete('http://localhost:8082/categorias/' + this.categoria.id).then(response => {
           this.fecharModal(3);
           this.buscarCategorias();
+          this.limparInputDoModal();
+          this.showAlertSucesso(2);
         }, response => {
-          alert("Erro ao excluir!")
+          this.mensagem = response.body.message;
+          this.setSegundosError(4);
         })
-
       },
-      atualizarNomeCategoria(evt) {
-        evt.preventDefault();
-        this.$http.put('http://localhost:8082/categorias', this.categorias).then(response => {
-          alert("Categoria Atualizada com sucesso!");
+      atualizarNomeCategoria() {
+        this.resetAlert();
+        console.log(this.categoria);
+        this.$http.put('http://localhost:8082/categorias', this.categoria).then(response => {
           this.fecharModal(1);
           this.buscarCategorias();
+          this.showAlertSucesso(3);
         }, response => {
-          alert("Erro ao Atualizar!")
+          this.mensagem = response.body.message;
+          this.setSegundosError(4);
         });
       },
       routerParaProdutos() {
         console.log(this.categoria.id);
-
         this.$router.push('/categorias/' + this.categoria.id + '/produtos')
       },
       onFiltered(filteredItems) {
-        // Trigger pagination to update the number of buttons/pages due to filtering
         this.totalRows = filteredItems.length;
         this.currentPage = 1
+      },
+      setSegundos(segundos) {
+        this.alertSucesso = segundos
+      },
+      setSegundosError(segundos) {
+        this.alertError = segundos;
+      },
+      showAlertSucesso(opcao) {
+        if (opcao === 1) {
+          this.alertSucesso = this.dismissSecs;
+          this.flagSalvar = true;
+        } else if (opcao === 2) {
+          this.alertSucesso = this.dismissSecs;
+          this.flagExcluir = true;
+        } else if (opcao === 3) {
+          this.alertSucesso = this.dismissSecs;
+          this.flagAtualizar = true;
+        } else if (opcao === 4) {
+          this.alertSucesso = this.dismissSecs;
+        }
+      },
+      resetAlert() {
+        this.flagSalvar = false;
+        this.flagExcluir = false;
+        this.flagAtualizar = false;
       }
     }
   }
 </script>
-
 
 <style scoped>
   .botao-categoria {
     color: white;
     border: none;
   }
-
   .botao-criar-categoria {
     background-color: green;
   }
-
   .grupo-botao-modal {
     float: right;
   }
-  .botao-cadastro{
+  .botao-cadastro {
     float: right;
     margin-bottom: 2%;
   }
-  .titulo-tabela{
+  .titulo-tabela {
     float: left;
   }
-  .card{
-    position: relative;
+  .card {
+    position: relative;margin-top: 10%;
   }
-  .barra-de-paginas{
+  .barra-de-paginas {
     position: relative;
-  }
-  .mostra-modal-nome-categoria-para-atualizar {
-    font-size: 30px;
-    float: left;
   }
   .row {
     margin-right: 0px;
   }
-  .card {
-    margin-top: 10%;
-  }
+
 </style>
--- src/components/Produto.vue
+++ src/components/Produto.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="row">
     <b-container class="col-md-6 offset-3" fluid>
-        <b-card class="card" bg-variant="dark" text-variant="white">
-
-          <h2 class="titulo-tabela">Categoria: {{corpoDosProdutos.nome}}</h2>
-
-          <!--CADASTRAR PRODUTO-->
-          <div class="botao-cadastro">
-            <b-input-group-append>
-              <b-button class="botao-criar-categoria"
-                        v-b-modal.modal-cadastrar>
-                <i class="fas fa-plus-circle"> Cadastrar Produto</i>
-              </b-button>
-            </b-input-group-append>
-
-          </div>
-
+      <b-card class="card" bg-variant="dark" text-variant="white">
+        <h2 class="titulo-tabela">Categoria: {{corpoDosProdutos.nome}}</h2>
+        <!--CADASTRAR PRODUTO-->
+        <div class="botao-cadastro">
+          <b-input-group-append>
+            <b-button class="botao-criar-categoria"
+                      v-b-modal.modal-cadastrar>
+              <i class="fas fa-plus-circle"> Cadastrar Produto</i>
+            </b-button>
+          </b-input-group-append>
+        </div>
+        <div style="height: 350px">
           <b-table class="hover"
                    :items="corpoDosProdutos.produtos"
-                   :fields="campoParaColunasDosProdutos">
+                   :fields="campoParaColunasDosProdutos"
+                   :current-page="currentPage"
+                   @filtered="onFiltered(item)"
+                   :per-page="perPage">
 
-
+            <template slot="preco" slot-scope="row">
+              <p>{{corpoDosProdutos.produtos[row.index].preco}}</p>
+            </template>
             <template slot="ação" slot-scope="row">
-
               <!--BOTAO PARA ATUALIZAR PRODUTO-->
               <b-button v-b-modal.modal-editar
                         title="Editar Categoria"
@@ -30,7 +31,6 @@
                         variant="outline-primary"
                         @click="mandaValorDaLinhaParaModal(row.item)"><i class="fas fa-edit"></i>
               </b-button>
-
               <!--BOTAO PARA EXCLUIR PRODUTO-->
               <b-button v-b-modal.modal-excluir
                         title="Excluir Produto"
@@ -38,52 +38,44 @@
                         variant="outline-primary"
                         @click="mandaValorDaLinhaParaModal(row.item)"><i class="fas fa-trash-alt"></i>
               </b-button>
-
             </template>
-
           </b-table>
+          <!--BOTAO VOLTAR PARA CATEGORIA-->
+        </div>
+        <div class="botao-cadastro ">
+          <b-input-group-append>
+            <b-button class="botao-voltar-para-categoria"
+                      v-b-modal.modal-cadastrar
+                      to="/categorias"
+            ><i class="fas fa-arrow-alt-circle-left"></i>
+            </b-button>
+          </b-input-group-append>
+        </div>
 
 
-          <div class="botao-cadastro ">
-            <b-input-group-append>
-              <b-button class="botao-voltar-para-categoria"
-                        v-b-modal.modal-cadastrar
-                        to="/categorias"
-              ><i class="fas fa-arrow-alt-circle-left"></i>
-              </b-button>
-            </b-input-group-append>
-          </div>
-
-
-          <!--PAGINAS-->
-          <div class="barra-de-paginas">
-            <b-row>
-              <b-col md="6" class="my-1">
-                <b-pagination
-                  v-model="currentPage"
-                  :total-rows="totalRows"
-                  :per-page="perPage"
-                  class="my-0"
-                ></b-pagination>
-              </b-col>
-            </b-row>
-
-          </div>
-
-
-        </b-card>
+        <!--PAGINAS-->
+        <div class="barra-de-paginas">
+          <b-row>
+            <b-col md="6" class="my-1">
+              <b-pagination
+                v-model="currentPage"
+                :total-rows="totalRows"
+                :per-page="perPage"
+                class="my-0"
+              ></b-pagination>
+            </b-col>
+          </b-row>
+        </div>
+      </b-card>
     </b-container>
-
     <!--MODAL CADASTRAR-->
     <div>
       <b-modal id="modal-cadastrar" title="Cadastrar Produto" hide-footer ref="modal-cadastrar-produto">
         <div>
           <b-form @submit="cadastrarProduto" @reset="limparInputDoModal">
-
             <b-form-group class="col-md-6 offset-3"
                           label-for="input-1"
                           description="Não coloque nome inapropriado.">
-
               <b-form-input
                 type="text"
                 required
@@ -96,18 +88,14 @@
                 placeholder="Preço do Produto"
                 v-model="produto.preco"></b-form-input>
             </b-form-group>
-
-
             <div class="grupo-botao-modal">
               <b-button type="reset" variant="danger" @click="fecharModal(2)">Cancelar</b-button>
               <b-button type="submit" variant="success">Pronto</b-button>
             </div>
-
           </b-form>
         </div>
       </b-modal>
     </div>
-
     <!--MODAL-ATUALIZAR-->
     <div>
       <b-modal id="modal-editar" title="Atualizar Produto" hide-footer ref="modal-atualizar-produto">
@@ -118,7 +106,6 @@
               label="Nome: "
               label-for="input-1"
               description="Não coloque nome inapropriado.">
-
               <b-form-input
                 type="text"
                 required
@@ -132,7 +119,6 @@
                 required
                 v-model="produto.preco"
               ></b-form-input>
-
             </b-form-group>
 
             <div class="grupo-botao-modal">
@@ -150,8 +136,6 @@
         </div>
       </b-modal>
     </div>
-
-
     <!--MODAL EXCLUIR-->
     <div>
       <b-modal id="modal-excluir" title="Excluir Produto" hide-footer ref="modal-excluir-produto">
@@ -163,13 +147,34 @@
               <b-button type="reset" variant="danger" @click="fecharModal(3)">Cancelar</b-button>
               <b-button type="submit" variant="success">Pronto</b-button>
             </div>
-
           </b-form>
         </div>
       </b-modal>
     </div>
-
-
+    <!--ALERT SUCESSO-->
+    <div style="position: absolute;right: 10px;top: 10px;z-index: 100000">
+      <b-alert
+        :show="alertSucesso"
+        dismissible
+        variant="success"
+        @dismissed="alertSucesso=0"
+        @dismiss-count-down="setSegundos">
+        <p v-show="flagSalvar">Produto salva com sucesso!</p>
+        <p v-show="flagExcluir">Produto Excluida com sucesso!</p>
+        <p v-show="flagAtualizar">Produto Atualizada com sucesso!</p>
+      </b-alert>
+    </div>
+    <!--ALERT ERROR-->
+    <div style="position: absolute;right: 10px;top: 10px;z-index: 100000">
+      <b-alert
+        :show="alertError"
+        dismissible
+        variant="danger"
+        @dismissed="alertError=0"
+        @dismiss-count-down="setSegundos">
+        <p v-model="mensagem">{{mensagem}}</p>
+      </b-alert>
+    </div>
   </div>
 </template>
 
@@ -182,7 +187,7 @@
       return {
         totalRows: 1,
         currentPage: 1,
-        perPage: 5,
+        perPage: 4,
         categoriaId: '',
         corpoDosProdutos: [],
         campoParaColunasDosProdutos: [{key: 'nome'}, {key: 'preco'}, {key: 'ação'}],
@@ -190,61 +195,68 @@
           id: '',
           nome: '',
           preco: null
-        }
+        },
+        mensagem: '',
+        dismissSecs: 10,
+        alertSucesso: 0,
+        alertError: 0,
+        flagSalvar: false,
+        flagExcluir: false,
+        flagAtualizar: false
       }
     },
     created() {
       this.buscarProdutos(this.$router.app._route.params.id);
-    },
-    mounted() {
-      // Set the initial number of items
-
-      this.totalRows = this.corpoDosProdutos.length;
-      console.log(this.totalRows)
     },
     methods: {
       buscarProdutos(id) {
         this.categoriaId = id;
         this.$http.get('http://localhost:8082/categorias/' + id).then(response => {
           this.corpoDosProdutos = response.body;
+          this.totalRows = this.corpoDosProdutos.produtos.length;
         });
       },
       buscarProdutosNovamente() {
+
         this.$http.get('http://localhost:8082/categorias/' + this.categoriaId).then(response => {
           this.corpoDosProdutos = response.body;
+          this.totalRows = this.corpoDosProdutos.produtos.length;
         });
       },
       cadastrarProduto() {
-        console.log(this.produto);
+        this.resetAlert();
         this.$http.post('http://localhost:8082/categorias/' + this.categoriaId + '/produtos', this.produto).then(response => {
-          alert("Produto Cadastrado com sucesso!");
           this.fecharModal(2);
+          this.limparInputDoModal();
           this.buscarProdutosNovamente();
+          this.showAlertSucesso(1);
         }, response => {
-          alert("Erro ao Cadastrar!")
+          this.mensagem = response.body.message;
+          this.setSegundosError(4);
         });
-
-
       },
       atualizarProduto() {
+        this.resetAlert();
         this.$http.put('http://localhost:8082/categorias/' + this.categoriaId + '/produtos', this.produto).then(response => {
-          alert("Produto Atualizado com Sucesso!");
           this.fecharModal(1);
           this.buscarProdutosNovamente();
+          this.limparInputDoModal();
+          this.showAlertSucesso(3);
         }, response => {
-          alert("Erro ao Atualizar!")
+          this.mensagem = response.body.message;
+          this.setSegundosError(4);
         });
-
       },
       excluirProduto() {
+        this.resetAlert();
         this.$http.delete('http://localhost:8082/categorias/' + this.categoriaId + '/produtos', {body: this.produto}).then(response => {
-          alert("Produto Excluido com Sucesso!");
           this.fecharModal(3);
           this.buscarProdutosNovamente();
+          this.showAlertSucesso(2);
         }, response => {
-          alert("Erro ao excluir!");
+          this.mensagem = response.body.message;
+          this.setSegundosError(4);
         })
-
       },
       mandaValorDaLinhaParaModal(item) {
         this.produto.id = item.id;
@@ -263,8 +275,36 @@
       limparInputDoModal() {
         this.produto.nome = '';
         this.produto.preco = '';
-
-      }
+      },
+      showAlertSucesso(opcao) {
+        if (opcao === 1) {
+          this.alertSucesso = this.dismissSecs;
+          this.flagSalvar = true;
+        } else if (opcao === 2) {
+          this.alertSucesso = this.dismissSecs;
+          this.flagExcluir = true;
+        } else if (opcao === 3) {
+          this.alertSucesso = this.dismissSecs;
+          this.flagAtualizar = true;
+        } else if (opcao === 4) {
+          this.alertSucesso = this.dismissSecs;
+        }
+      },
+      resetAlert() {
+        this.flagSalvar = false;
+        this.flagExcluir = false;
+        this.flagAtualizar = false;
+      },
+      onFiltered(filteredItems) {
+        this.totalRows = filteredItems.length;
+        this.currentPage = 1
+      },
+      setSegundos(segundos) {
+        this.alertSucesso = segundos
+      },
+      setSegundosError(segundos) {
+        this.alertError = segundos;
+      },
     }
   }
 </script>
@@ -297,13 +337,14 @@
   }
 
   .card {
-    position: absolute;
+    margin-top: 10%;
+    position: relative;
   }
 
   .barra-de-paginas {
     position: relative;
-
   }
+
   .row {
     margin-right: 0px;
   }
